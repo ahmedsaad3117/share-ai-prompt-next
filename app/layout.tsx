eb5211b66d2c44d201b91f422fd736102129cccd
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-07-13 01:08:09 +0300

    feat: nav bar for mobile and desktop

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css'
 
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
 export const metadata = {
     title: "AI Prompts",
     description: "Discover & Share AI Prompts"
@@ -14,7 +17,7 @@ const RootLayout = ({children} : any) => {
                 </div>
 
                 <main className='app'>
-                    {/* @ts-ignore */}
+                    <Nav/>
                     {children}
                 </main>
             </body>
