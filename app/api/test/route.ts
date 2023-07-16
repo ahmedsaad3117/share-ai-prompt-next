89057856f109a8964e11b9a63b95f338231b576c
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-07-17 01:06:56 +0300

    feat: create the first prompt and save it to DB

---
+++ app/api/test/route.ts
@@ -0,0 +1,7 @@
+import Prompt from "@models/Prompt"
+
+export const GET = async (req,res) =>{
+    const prompts = await Prompt.find({})
+    console.log(prompts)
+    return new Response("this a new ")
+}
