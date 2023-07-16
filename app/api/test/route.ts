d67a7ddca31e58e128a3db2714b2f2ef67042c5d
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-07-17 02:47:47 +0300

    feat: feed page with all prompts on it

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,6 @@
 import Prompt from "@models/Prompt"
 
-export const GET = async (req,res) =>{
+export const GET = async (req, res) => {
     const prompts = await Prompt.find({})
     console.log(prompts)
     return new Response("this a new ")
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }: any) => {
+  console.log(data, "data")
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post: any) => (
+        <PromptCard
+          key={post._id}
+          // @ts-ignore
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>);
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
 
-export default Feed
+  // @ts-ignore
+  const handleSearchChange = (e) => {
+    e.preventDefualt();
+
+    // SetSearchText(e.)
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      console.log("data", data)
+      // @ts-ignore
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full felx-center">
+        <input
+          type="text"
+          placeholder="Search for prompts"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => { }} />
+    </section>
+  );
+};
+
+export default Feed;
